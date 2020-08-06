19ce4509172716fc90e8563c51e5701149b2b150
Ulimn <fema.90@gmail.com>
2020-08-06 09:01:27 +0000

    Fix formatting

--- insertion-sort.c
+++ insertion-sort.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
 
-int main(void)
-{
-	int arr[] = {2, 8, 4, 1},
-			n = sizeof(arr) / sizeof(arr[0]),
-			x;
+int main(void) {
+  int arr[] = {2, 8, 4, 1}, n = sizeof(arr) / sizeof(arr[0]), x;
 
-	if (n <= 1)
-	{
-		return 0;
-	}
+  if (n <= 1) {
+    return 0;
+  }
 
-	for (int i = 1; i < n; i++)
-	{
-		x = arr[i];
-		for (int j = i; j > 0 && arr[j] < arr[j-1]; j--)
-		{
-            int y = arr[j];
-            arr[j] = arr[j-1];
-            arr[j-1] = y;
-		}
-	}
+  for (int i = 1; i < n; i++) {
+    x = arr[i];
+    for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+      int y = arr[j];
+      arr[j] = arr[j - 1];
+      arr[j - 1] = y;
+    }
+  }
 
-	for (int q = 0; q < n; q++)
-	{
-		printf("%d ", arr[q]);
-	}
-	printf("\n");
+  for (int q = 0; q < n; q++) {
+    printf("%d ", arr[q]);
+  }
+  printf("\n");
 
-	return 0;
+  return 0;
 }
